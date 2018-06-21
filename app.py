8ef60a80d7842dea247fca9123c546fff14568fc
Kang Ming Tay <kang.ming1996@gmail.com>
2018-06-21 12:25:10 +0800

    error codes

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import json
 
 from flask import Flask, request, make_response, jsonify
 
-#from forecast import Forecast, validate_params
 
 app = Flask(__name__)
 log = app.logger
@@ -17,23 +16,20 @@ def webhook():
         action = req.get('queryResult').get('action')
     except AttributeError:
         return 'json error'
-    """
-    KEY IN ACTIONS HERE
 
-    if action == 'weather':
-        res = weather(req)
-    elif action == 'weather.activity':
-        res = weather_activity(req)
-    elif action == 'weather.condition':
-        res = weather_condition(req)
-    elif action == 'weather.outfit':
-        res = weather_outfit(req)
-    elif action == 'weather.temperature':
-        res = weather_temperature(req)
+    if action == 'ErrorCodes.ErrorCodes-custom':
+        res = find_error_code(req)
+
     else:
         log.error('Unexpected action.')
-    """
+
     print('Action: ' + action)
     print('Response: ' + res)
 
     return make_response(jsonify({'fulfillmentText': res}))
+
+
+def find_error_code(req):
+    code = req['queryResult']['parameters']['number']
+    if code == '00000':
+        return 'Payment Successful'
